f65848b5d51fee951e392fd9950bc8039451b6f3
Marcin Owerczuk <muzieh@gmail.com>
2016-08-02 08:21:06 +0100

    bundle.js removed from git, hash based routing activated

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, Router, hashHistory} from 'react-router';
+
 import Voting from './components/Voting';
+import App from './components/App';
+import Results from './components/Results';
 
 
-const pair = ['Trainspotting', 'Blade Runner'];
+const routes = <Route component={App}>
+	<Route path="/results" component={Results} />
+	<Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
-	<Voting pair={pair} />,
-
+	<Router history={hashHistory}>{routes}</Router>,
 	document.getElementById('app')
 );
+
