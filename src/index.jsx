f318600f5184de440109970ac7b07a3fdcd89838
Marcin Owerczuk <muzieh@gmail.com>
2016-08-10 10:14:18 +0200

    app.js and index.jsx from mac before merging

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router} from 'react-router';
 import Voting from './components/Voting';
+import App from './components/App';
 
 
 const pair = ['Trainspotting', 'Blade Runner'];
+
+const routes = 
 
 ReactDOM.render(
 	<Voting pair={pair} />,
